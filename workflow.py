9196d0c8c265f8969c9509cc93b36112f4a7f016
Marcin Wierzbiński <marcin119a@gmail.com>
2026-02-07 14:54:17 +0100

    openrouter

--- workflow.py
+++ workflow.py
@@ -1,5 +1,6 @@
+import os
 from langgraph.graph import MessagesState, StateGraph, START, END
-from langchain.chat_models import init_chat_model
+from langchain_openai import ChatOpenAI
 from retriever import get_retriever
 from langchain_core.tools import create_retriever_tool
 from pydantic import BaseModel, Field
@@ -7,8 +8,18 @@ from typing import Literal
 from langchain_core.messages import HumanMessage
 from langgraph.prebuilt import ToolNode, tools_condition
 
-response_model = init_chat_model(model="gpt-4o", temperature=0.0)
-grader_model = init_chat_model(model="gpt-4o-mini", temperature=0.0)
+response_model = ChatOpenAI(
+    model="openai/gpt-4o",
+    temperature=0.0,
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url=os.getenv("OPENROUTER_BASE_URL"),
+)
+grader_model = ChatOpenAI(
+    model="deepseek/deepseek-v3.2-speciale",
+    temperature=0.0,
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url=os.getenv("OPENROUTER_BASE_URL"),
+)
 
 retriever = get_retriever()
 retriever_tool = create_retriever_tool(
@@ -101,7 +112,4 @@ workflow.add_edge("rewrite_question", "generate_query_or_respond")
 graph = workflow.compile()
 
 result = (graph.invoke({"messages": [HumanMessage(content="Jak zainstalować Docker Desktop na Linuxie?")]}))
-print(result["messages"][-1].content)
-
-result = graph.invoke({"messages": [HumanMessage(content="Jak zainstalować Docker Desktop na Linuxie?")]})
 print(result["messages"][-1].content)
